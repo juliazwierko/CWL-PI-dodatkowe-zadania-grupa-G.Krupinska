a5edb1436adf11883bc55d4791fe2bc5c78c80ba
Julia Zwierko <114949901+juliazwierko@users.noreply.github.com>
2022-12-25 16:39:55 +0100

    Create 7.c

---
+++ 7.c
@@ -0,0 +1,56 @@
+/*
+     5 43210
+    54 3210
+   543 210
+  5432 10
+ 54321 0
+543210 
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+
+int main() 
+{   
+    int liczba;
+    printf("Wpisz licbe: ");
+    scanf("%d", &liczba);
+
+    int wysokosc = liczba+1;
+    printf("Wysokosc wynosi: %d\n", wysokosc);
+    int szerokosc = (wysokosc*2)+1;
+    printf("Szerokosc wynos: %d\n", szerokosc);
+    int tmp = liczba;
+
+    printf("\n");
+    for(int i=0; i<wysokosc; i++)
+    {   
+        liczba = tmp;
+        for(int j=0; j<(wysokosc-i-1); j++)
+        {
+            printf(" ");
+        }
+        for(int j=0; j<=i; j++)
+        {
+            printf("%d", liczba);
+            liczba --;
+        }
+        
+        for(int j=0; j<1; j++)
+        {
+            printf(" ");
+        }
+
+        for(int j=0; j<wysokosc-i-1; j++)
+        {   
+            printf("%d", liczba);
+            liczba--;
+        }
+
+        printf("\n");
+    }
+
+
+return 0;
+}
