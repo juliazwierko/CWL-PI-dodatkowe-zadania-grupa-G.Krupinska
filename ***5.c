a9199b892573a9a59406aa50a61c81b77a686a1e
Julia Zwierko <114949901+juliazwierko@users.noreply.github.com>
2022-12-25 13:35:49 +0100

    Update ***5.c

--- ***5.c
+++ ***5.c
@@ -7,48 +7,72 @@
          A 
 (przyklad dla literki F)
 */
-
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 #include <math.h>
 
-int main() 
+int main()
 {
-    int wysokosc = 6;
+    char literka;
+    printf("Wpisz literke: ");
+    scanf("%c", &literka);
+
+    int szerokosc = ((2*(literka-64)) - 1);
+    printf("Szerokośc trojkata wynoci: %d\n", szerokosc); //11
+    
+    int wysokosc = (literka-64);
+    printf("Wysokosc trojkata wynosi: %d\n", wysokosc); //6
+
+    
     for(int i=0; i<wysokosc; i++)
-    {   
-        for(int j = wysokosc; j>wysokosc - i; j -= 1)
-        {
-            printf("_");
-        }
-        for(int j = 0; j<wysokosc - i; j++)
-        {
-            printf("*");
-        }
-
-        for(int j = (wysokosc); j<(wysokosc*2)-i; j += 1)
-        {
-            printf("*");
-        }
-
-        for(int j = (wysokosc*2); j>(wysokosc*2)-i; j -= 1)
+    {
+        char zmianaliterki = 'A';
+        for(int j=0; j<i; j++)
         {
             printf("_");
         }
 
+        for(int j=0; j<wysokosc-i; j++)
+        {
+            printf("%c", zmianaliterki);
+            zmianaliterki++; 
+        }
+        zmianaliterki -= 2;
+        for(int j=0; j<(szerokosc-wysokosc)-i ; j++)
+        {
+            printf("%c", zmianaliterki);
+            zmianaliterki--;
+        }
+        for(int j=0; j<i; j++)
+        {
+            printf("_");
+        }
         printf("\n");
     }
-    printf("\n");
 
+    printf("\n");
     return 0;
 }
 
 /*
-************
-_**********_
-__********__
-___******___
-____****____
-_____**_____
+ABCDEFGHIJKLMNOPQRSTUTSRQPONMLKJIHGFEDCBA
+_ABCDEFGHIJKLMNOPQRSTSRQPONMLKJIHGFEDCBA_
+__ABCDEFGHIJKLMNOPQRSRQPONMLKJIHGFEDCBA__
+___ABCDEFGHIJKLMNOPQRQPONMLKJIHGFEDCBA___
+____ABCDEFGHIJKLMNOPQPONMLKJIHGFEDCBA____
+_____ABCDEFGHIJKLMNOPONMLKJIHGFEDCBA_____
+______ABCDEFGHIJKLMNONMLKJIHGFEDCBA______
+_______ABCDEFGHIJKLMNMLKJIHGFEDCBA_______
+________ABCDEFGHIJKLMLKJIHGFEDCBA________
+_________ABCDEFGHIJKLKJIHGFEDCBA_________
+__________ABCDEFGHIJKJIHGFEDCBA__________
+___________ABCDEFGHIJIHGFEDCBA___________
+____________ABCDEFGHIHGFEDCBA____________
+_____________ABCDEFGHGFEDCBA_____________
+______________ABCDEFGFEDCBA______________
+_______________ABCDEFEDCBA_______________
+________________ABCDEDCBA________________
+_________________ABCDCBA_________________
+__________________ABCBA__________________
+___________________ABA___________________
+____________________A____________________
 */
